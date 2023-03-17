51bb48fc7a4d3eeb47d5081fa145f31ea870db62
Yeonghwan Park <vyckd354@gmail.com>
2023-03-17 16:25:08 -0500

    Clean some Cypress test codes and owl and screen img tweaked for responsive

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,87 +1,91 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 describe("E2E Test", () => {
-  it("Create and Delete both a Day and word from Memorize and video", () => {
-    // Login
-    cy.viewport(1500, 880);
-    cy.visit("http://localhost:3000/");
-    cy.findByPlaceholderText(/.*Email.*/i).type("test123@gmail.com");
-    cy.findByPlaceholderText(/.*Password.*/i).type("test123");
-    cy.findByText(/.*Sign in.*/i).click();
-    cy.wait(1000);
+  it(
+    "Create and Delete both a Day and word from Memorize and video",
+    { baseUrl: "http://localhost:3000" },
+    () => {
+      // Login
+      cy.viewport(1500, 880);
+      cy.visit("/");
+      cy.findByPlaceholderText(/.*Email.*/i).type("test123@gmail.com");
+      cy.findByPlaceholderText(/.*Password.*/i).type("test123");
+      cy.findByText(/.*Sign in.*/i).click();
+      cy.wait(1000);
 
-    // Create a Day
-    cy.visit("http://localhost:3000/memorize");
-    cy.findByRole("button", {
-      name: /.*add day.*/i,
-    }).click();
-    cy.findByRole("button", {
-      name: /.*confirm.*/i,
-    }).click();
-    cy.findByText(/day 1/i).should("exist");
+      // Create a Day
+      cy.visit("/memorize");
+      cy.findByRole("button", {
+        name: /.*add day.*/i,
+      }).click();
+      cy.findByRole("button", {
+        name: /.*confirm.*/i,
+      }).click();
+      cy.findByText(/day 1/i).should("exist");
 
-    // Enter the Day 1 and Create a word
-    cy.findByRole("link", {
-      name: /.*day 1.*/i,
-    }).click();
-    cy.findByRole("button", {
-      name: /.*create word.*/i,
-    }).click();
+      // Enter the Day 1 and Create a word
+      cy.findByRole("link", {
+        name: /.*day 1.*/i,
+      }).click();
+      cy.findByRole("button", {
+        name: /.*create word.*/i,
+      }).click();
 
-    cy.findByRole("combobox").select("1");
-    cy.findByPlaceholderText(/.*사과.*/i).type("사과");
-    cy.findByPlaceholderText(/.*Apple.*/i).type("Apple");
-    cy.findByRole("button", {
-      name: /.*create.*/i,
-    }).click();
+      cy.findByRole("combobox").select("1");
+      cy.findByPlaceholderText(/.*사과.*/i).type("사과");
+      cy.findByPlaceholderText(/.*Apple.*/i).type("Apple");
+      cy.findByRole("button", {
+        name: /.*create.*/i,
+      }).click();
 
-    // Check if the word that I just created exist
-    cy.findByText(/.*사과.*/i).should("exist");
-    cy.findByText(/.*Apple.*/i).should("exist");
+      // Check if the word that I just created exist
+      cy.findByText(/.*사과.*/i).should("exist");
+      cy.findByText(/.*Apple.*/i).should("exist");
 
-    // Delete the word that I created
-    cy.findByRole("button", {
-      name: /x/i,
-    }).click();
-    cy.wait(2000);
-    cy.findByRole("button", {
-      name: /.*delete*/i,
-    }).click();
+      // Delete the word that I created
+      cy.findByRole("button", {
+        name: /x/i,
+      }).click();
+      cy.wait(2000);
+      cy.findByRole("button", {
+        name: /.*delete*/i,
+      }).click();
 
-    // Create a word from Video
-    cy.visit("http://localhost:3000/video");
-    cy.findByRole("button", {
-      name: /.*See create.*/i,
-    }).click();
+      // Create a word from Video
+      cy.visit("/video");
+      cy.findByRole("button", {
+        name: /.*See create.*/i,
+      }).click();
 
-    cy.findByRole("combobox").select("1");
-    cy.findByPlaceholderText(/.*사과.*/i).type("비디오");
-    cy.findByPlaceholderText(/.*Apple.*/i).type("Video");
-    cy.findByTestId("createBtn").click();
-    cy.wait(3000);
+      cy.findByRole("combobox").select("1");
+      cy.findByPlaceholderText(/.*사과.*/i).type("비디오");
+      cy.findByPlaceholderText(/.*Apple.*/i).type("Video");
+      cy.findByTestId("createBtn").click();
+      cy.wait(3000);
 
-    // Check if the word that I just created exist
-    cy.visit("http://localhost:3000/memorize/1");
-    cy.wait(3000);
-    cy.findByText(/.*비디오.*/i).should("exist");
+      // Check if the word that I just created exist
+      cy.visit("/memorize/1");
+      cy.wait(3000);
+      cy.findByText(/.*비디오.*/i).should("exist");
 
-    // Delete the word that I created
-    cy.findByRole("button", {
-      name: /x/i,
-    }).click();
-    cy.wait(2000);
-    cy.findByRole("button", {
-      name: /.*delete*/i,
-    }).click();
+      // Delete the word that I created
+      cy.findByRole("button", {
+        name: /x/i,
+      }).click();
+      cy.wait(2000);
+      cy.findByRole("button", {
+        name: /.*delete*/i,
+      }).click();
 
-    // Delete the day
-    cy.findByText(/.*arrow_back_ios.*/i).click();
-    cy.findByRole("button", {
-      name: /.*delete day.*/i,
-    }).click();
-    cy.wait(4000);
-    cy.findByTestId("deleteBtn").click();
+      // Delete the day
+      cy.findByText(/.*arrow_back_ios.*/i).click();
+      cy.findByRole("button", {
+        name: /.*delete day.*/i,
+      }).click();
+      cy.wait(4000);
+      cy.findByTestId("deleteBtn").click();
 
-    // Logout
-    cy.findByText(/log out/i).click();
-  });
+      // Logout
+      cy.findByText(/log out/i).click();
+    }
+  );
 });
