e195249cee4331dc3a3511b80edd7f3c0e338192
Yeonghwan Park <vyckd354@gmail.com>
2023-08-30 01:46:17 +0900

    Tweaked some UI of NavBar

--- src/components/NavBar_NavModal/NavBar.js
+++ src/components/NavBar_NavModal/NavBar.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { authService } from "../../fBase";
-import styles from "./NavBar_NavModal.module.css";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { authService } from '../../fBase';
+import styles from './NavBar_NavModal.module.css';
 
 export default function NavBar({ setNavModal, userObj }) {
   // NavModal (click Hamburger Icon)
   function showModal() {
     setNavModal(true);
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   }
 
   // LogOut
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
-    navigate("/");
+    navigate('/');
   };
 
   // Scroll up to the top
@@ -24,51 +24,51 @@ export default function NavBar({ setNavModal, userObj }) {
 
   // Whenever scroll event happen, Change the value of scroll Y-coordinate
   const [scrollYValue, setScrollYValue] = useState(window.scrollY);
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     setScrollYValue(window.scrollY);
   });
 
   return (
     <div
       id={scrollYValue <= 500 ? styles.navbar : styles.navbarHidden}
-      className="drop-shadow-lg font-serif bg-gradient-to-r from-purple-50 via-indigo-50 to-pink-50"
+      className='drop-shadow-lg font-serif bg-gradient-to-r from-purple-50 via-indigo-50 to-pink-50'
     >
-      <div className="flex flex-row text-xl">
+      <div>
         {/* MYabc Logo and letter */}
-        <Link to="/" className="flex flex-row">
+        <Link to='/' className='flex flex-row'>
           <img
-            className="flex-none w-14 h-14"
+            className='flex-none w-14 h-14'
             onClick={scrollUp}
             src={
-              "https://user-images.githubusercontent.com/106279616/217299245-76306248-6c80-4bf8-a1f0-ccb962648a8f.png"
+              'https://user-images.githubusercontent.com/106279616/217299245-76306248-6c80-4bf8-a1f0-ccb962648a8f.png'
             }
-            width="50px"
-            alt="ABC_LOGO"
+            width='50px'
+            alt='ABC_LOGO'
           />
-          <span id={styles.myabc} className="grow h-14 self-center">
+          <span id={styles.myabc} className='grow h-14 self-center'>
             MYabc
           </span>
         </Link>
         {/* Menus */}
         <div className={styles.menus}>
-          <Link to="/memorize" className={styles.menu}>
-            <span className="material-icons-outlined">psychology</span>
-            <span role="link">Memorize </span>
+          <Link to='/memorize' className={styles.menu}>
+            <span className='material-icons-outlined'>psychology</span>
+            <span role='link'>Memorize </span>
           </Link>
           <span>|&nbsp;</span>
-          <Link to="/dictionary" className={styles.menu}>
-            <span className="material-icons-outlined">menu_book</span>
-            <span role="link">Dictionary</span>
+          <Link to='/dictionary' className={styles.menu}>
+            <span className='material-icons-outlined'>menu_book</span>
+            <span role='link'>Dictionary</span>
           </Link>
           <span>|&nbsp;</span>
-          <Link to="/video" className={styles.menu}>
-            <span className="material-icons-outlined">ondemand_video</span>
-            <span role="link">Video</span>
+          <Link to='/video' className={styles.menu}>
+            <span className='material-icons-outlined'>ondemand_video</span>
+            <span role='link'>Video</span>
           </Link>
           <span>|&nbsp;</span>
-          <Link to="/grammarlyapp" className={styles.menu}>
-            <span className="material-icons-outlined">sticky_note_2</span>
-            <span role="link">Grammarly</span>
+          <Link to='/grammarlyapp' className={styles.menu}>
+            <span className='material-icons-outlined'>sticky_note_2</span>
+            <span role='link'>Grammarly</span>
           </Link>
           {/* UserName, Logout button */}
           <div id={styles.nameLogout}>
@@ -76,8 +76,8 @@ export default function NavBar({ setNavModal, userObj }) {
             <button onClick={onLogOutClick}>
               <div
                 id={styles.logout}
-                className="rounded-lg transition shadow-gray-500/50 shadow-md ease-in-out
-                            delay-50 bg-gray-400 hover:-translate-y-1 hover:bg-gray-300 duration-300"
+                className='rounded-lg transition shadow-gray-500/50 shadow-md ease-in-out
+                            delay-50 bg-gray-400 hover:-translate-y-1 hover:bg-gray-300 duration-300'
               >
                 Log Out
               </div>
@@ -88,7 +88,7 @@ export default function NavBar({ setNavModal, userObj }) {
         <span
           onClick={showModal}
           id={styles.menuIcon}
-          className="cursor-pointer material-icons-outlined"
+          className='cursor-pointer material-icons-outlined'
         >
           menu
         </span>
